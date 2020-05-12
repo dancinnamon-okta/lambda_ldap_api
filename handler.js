317f7916921c55e1e70961095bbcf747b65c9bce
Dan Cinnamon <dan.cinnamon@okta.com>
2020-05-12 15:54:16 +0000

    Fixed error handling and cleaned up code.

--- handler.js
+++ handler.js
@@ -2,62 +2,33 @@
 
 module.exports.searchLdap = (event, context, callback) => {
 	var ldap = require('ldapjs')
-	var response = {
-		statusCode: 500,
-		body: 'Unknown error.'
-	}
-	var bodyData = {
-		message: undefined,
-		users: undefined,
-		userCount: 0
-	}
-
+	var assert = require('assert')
+	
 	var ldapHost = event.headers['ldapHost']
 	var bindDN = event.headers['bindDN']
 	var bindPW = event.headers['bindPW']
 	var searchBase = event.headers['searchBase']
 	var returnAttributes = event.headers['userAttributes']
-	
-	//Validate we have all of our context that we need to connect to the LDAP server.
-	if(ldapHost == undefined || 
-	bindDN == undefined || 
-	bindPW == undefined || 
-	searchBase == undefined || 
-	event.queryStringParameters == undefined) {
-		response.statusCode = 400
-		bodyData.message = 'Bad request. Not all required configuration attributes are properly configured. Please check your gateway configuration.'
-		response.body = JSON.stringify(bodyData)
-		callback(null, response)	 //Exit	status 400
-	}
+
+	assert(ldapHost, '[400] Bad request. Missing required header ldapHost. Please check your gateway configuration.')
+	assert(bindDN, '[400] Bad request. Missing required header bindDN. Please check your gateway configuration.')
+	assert(bindPW, '[400] Bad request. Missing required header bindPW. Please check your gateway configuration.')
+	assert(searchBase, '[400] Bad request. Missing required header searchBase. Please check your gateway configuration.')
+	assert(returnAttributes, '[400] Bad request. Missing required header returnAttributes. Please check your gateway configuration.')
+	assert(event.queryStringParameters.q, '[400] Bad request. Please include an ldap filter in the "q" querystring variable.')
 	
 	var client = ldap.createClient({
 		url: ('ldap://' + ldapHost + '/' + bindDN)
 	})
-	var users = []
 	
-	//Validate we have a filter.
-	if(event.queryStringParameters.q == undefined) {
-		response.statusCode = 400
-		bodyData.message = 'Bad request.  Please include an ldap filter in the "q" querystring variable.'
-		response.body = JSON.stringify(bodyData)
-		
-		callback(null, response) //Exit status 400
-	}
+	var users = []
+
 	client.bind(bindDN, bindPW, function(err) {
-		if(err != undefined) {
-			console.log(err)
-			response.statusCode = 500
-			bodyData.message = 'An error occured binding to the LDAP server:' + err
-			response.body = JSON.stringify(bodyData)
-		
-			client.unbind(function(err) {
-				console.log(err)
-			})
-			callback(null, response) //Exit status 500
-		}
+		console.log(err)
+		assert.ifError(err, '[500] An error occured binding to the LDAP server:' + err)
 		
 		var userAttributes = ['dn', 'samaccountname', 'userprincipalname']
-		if(returnAttributes != undefined && returnAttributes.split(',').length > 0 ) {
+		if(returnAttributes && returnAttributes.split(',').length > 0 ) {
 			userAttributes = returnAttributes.split(',')
 		}
 		var opts = {
@@ -79,11 +50,14 @@ module.exports.searchLdap = (event, context, callback) => {
 			})
 			
 			res.on('end', function(result) {
-				response.statusCode = 200
-				bodyData.users = users
-				bodyData.userCount = users.length
-				bodyData.message = 'Search Successful!'
-				response.body = JSON.stringify(bodyData)
+				const response = {
+					statusCode: 200,
+					body: JSON.stringify({
+						users: users,
+						userCount: users.length,
+						message: 'Search Successful!'
+					})
+				}
 				
 				client.unbind(function(err) {
 					console.log(err)
@@ -93,15 +67,12 @@ module.exports.searchLdap = (event, context, callback) => {
 			
 			res.on('error', function(err) {
 				console.log(err)
-				response.statusCode = 500
-				bodyData.message = 'An error occured searching for LDAP users:' + err
-				response.body = JSON.stringify(bodyData)
-
+				var message = '[500] An error occured searching for LDAP users:' + err
 				client.unbind(function(err) {
 					console.log(err)
 				})
-				callback(null, response) //Exit status 500
+				callback(new Error(message)) //Exit status 500
 			})
 		})
-	});
+	});	
 };
